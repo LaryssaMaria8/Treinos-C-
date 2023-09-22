c7a6aca1b7bb5ede2b9d6853d4028b4a14f96900
Laryssa Sena <131293968+LaryssaMaria8@users.noreply.github.com>
2023-09-21 21:23:48 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+Console.WriteLine("Quanto você deseja sacar? ");
+string saqueStr = Console.ReadLine();
+float saque;
+
+if (float.TryParse(saqueStr, out saque))
+{
+    float saldo = 800.0f;
+    float saldo_final = saldo - saque;
+    Console.WriteLine("Seu saldo final é: " + saldo_final);
+}
+else
+{
+    Console.WriteLine("Entrada inválida. Certifique-se de digitar um número válido.");
+}
